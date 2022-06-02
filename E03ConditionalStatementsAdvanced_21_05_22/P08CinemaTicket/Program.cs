fc743b2f2733dd36a2a7bbe294b946a6f3c3e302
VladislavNedyalkovIvanov <105384381+VladislavNedyalkovIvanov@users.noreply.github.com>
2022-06-02 14:54:09 +0300

    Add files via upload

---
+++ E03ConditionalStatementsAdvanced_21_05_22/P08CinemaTicket/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace P08CinemaTicket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayFromWeek = Console.ReadLine();
+            int price = 0;
+
+            if (dayFromWeek == "Monday")
+            {
+                price = 12;
+            }
+            else if (dayFromWeek == "Tuesday")
+            {
+                price = 12;
+            }
+            else if (dayFromWeek == "Wednesday")
+            {
+                price = 14;
+            }
+            else if (dayFromWeek == "Thursday")
+            {
+                price = 14;
+            }
+            else if (dayFromWeek == "Friday")
+            {
+                price = 12;
+            }
+            else if (dayFromWeek == "Saturday")
+            {
+                price = 16;
+            }
+            else if (dayFromWeek == "Sunday")
+            {
+                price = 16;
+            }
+            Console.WriteLine(price);
+        }
+    }
+}
