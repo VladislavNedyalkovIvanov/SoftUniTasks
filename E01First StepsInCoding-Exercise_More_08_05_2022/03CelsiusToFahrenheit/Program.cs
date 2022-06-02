a1cef9b92bd22640a1c958df775010a8bf3e65b6
VladislavNedyalkovIvanov <105384381+VladislavNedyalkovIvanov@users.noreply.github.com>
2022-06-02 14:46:28 +0300

    Add files via upload

---
+++ E01First StepsInCoding-Exercise_More_08_05_2022/03CelsiusToFahrenheit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03CelsiusToFahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Напишете програма, която чете градуси по скалата на Целзий (°C)
+            // и ги преобразува до градуси по скалата на Фаренхайт (°F).
+            // °F = °C × 1.8 + 32
+            // Форматирате изхода до втория знак след десетичната запетая. 
+
+            double celsius = double.Parse(Console.ReadLine());
+            double faren = celsius * 1.8 + 32;
+
+            Console.WriteLine($"{faren:f2}");
+
+        }
+    }
+}
---
+++ E01First StepsInCoding-Exercise_More_08_05_2022/P08CircleAreaPerimeter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P08CircleAreaPerimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberR = double.Parse(Console.ReadLine());
+            double circleS = Math.PI * (numberR * numberR);
+
+            double circleD = Math.PI * (numberR * 2);
+
+            Console.WriteLine($"{circleS:f2}");
+            Console.WriteLine($"{circleD:f2}");
+        }
+    }
+}
