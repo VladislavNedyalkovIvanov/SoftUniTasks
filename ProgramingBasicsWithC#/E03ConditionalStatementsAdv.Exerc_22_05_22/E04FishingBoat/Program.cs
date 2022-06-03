45d40b5f1f9975ee90271170839b65592be915d3
VladislavNedyalkovIvanov <105384381+VladislavNedyalkovIvanov@users.noreply.github.com>
2022-06-03 10:11:07 +0300

    Update Program.cs

--- ProgramingBasicsWithC#/E03ConditionalStatementsAdv.Exerc_22_05_22/E04FishingBoat/Program.cs
+++ ProgramingBasicsWithC#/E03ConditionalStatementsAdv.Exerc_22_05_22/E04FishingBoat/Program.cs
@@ -10,48 +10,51 @@ namespace E04FishingBoat
             string season = Console.ReadLine();
             int numberOfFishers = int.Parse(Console.ReadLine());
 
-            decimal price = 0m;
-            decimal discount = 0m;
-            decimal totalPrice = 0m;
+            double price = 0;
+            double discount = 0;
+            double totalPrice = 0;
+
 
             switch(season)
             {
                 case "Spring":
-                    price = 3000m;
+                    price = 3000;
                     break;
                 case "Summer":
-                    price = 4200m;
+                    price = 4200;
                     break;
                 case "Autumn":
-                    price = 4200m;
+                    price = 4200;
                     break;
                 case "Winter":
-                    price = 2600m;
+                    price = 2600;
                     break;
+
             }
+
 
             if (numberOfFishers <= 6)
             {
-                discount = price * 0.10m;
+                discount = price * 0.10;
             }
             else if (numberOfFishers >= 7 && numberOfFishers <= 11)
             {
-                discount = price * 0.15m;
+                discount = price * 0.15;
             }
             else if (numberOfFishers >= 12)
             {
-                discount = price * 0.25m;
+                discount = price * 0.25;
             }
 
 
             totalPrice = price - discount;
-            decimal bonusDiscount = 0m;
+            double bonusDiscount = 0;
 
             if (season != "Autumn")
             {
                 if (numberOfFishers % 2 == 0)
                 {
-                    bonusDiscount = totalPrice * 0.05m;
+                    bonusDiscount = totalPrice * 0.05;
                 }
             }
 
@@ -61,12 +64,12 @@ namespace E04FishingBoat
             {
                 if (totalPrice <= boudget)
                 {
-                    decimal allLeft = boudget - totalPrice;
+                    double allLeft = boudget - totalPrice;
                     Console.WriteLine($"Yes! You have {allLeft:f2} leva left.");
                 }
                 else if (totalPrice > boudget)
                 {
-                    decimal allNeeds = totalPrice - boudget;
+                    double allNeeds = totalPrice - boudget;
                     Console.WriteLine($"Not enough money! You need {allNeeds:f2} leva.");
                 }    
             }
