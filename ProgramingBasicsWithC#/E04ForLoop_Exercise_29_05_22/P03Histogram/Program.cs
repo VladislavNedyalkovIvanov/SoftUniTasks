11fca5417d3d631803a1f450f19e1c7c9074ccc7
VladislavNedyalkovIvanov <105384381+VladislavNedyalkovIvanov@users.noreply.github.com>
2022-06-10 11:50:20 +0300

    Add files via upload

---
+++ ProgramingBasicsWithC#/E04ForLoop_Exercise_29_05_22/P03Histogram/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace P03Histogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int numbersTo200 = 0;
+            int numbers200to399 = 0;
+            int numbers400to599 = 0;
+            int numbers600to799 = 0;
+            int numbersUpFrom800 = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int currentNumb = int.Parse(Console.ReadLine());
+                if (currentNumb < 200)
+                {
+                    numbersTo200 += 1;
+                }
+                else if (currentNumb >= 200 && currentNumb < 400)
+                {
+                    numbers200to399 += 1;
+                }
+                else if (currentNumb >= 400 && currentNumb < 600)
+                {
+                    numbers400to599 += 1;
+                }
+                else if (currentNumb >= 600 && currentNumb < 800)
+                {
+                    numbers600to799 += 1;
+                }
+                else
+                {
+                    numbersUpFrom800 += 1;
+                }
+            }
+            double percent1 = ((double)numbersTo200 / n) * 100;
+            double percent2 = ((double)numbers200to399 / n) * 100;
+            double percent3 = ((double)numbers400to599 / n) * 100;
+            double percent4 = ((double)numbers600to799 / n) * 100;
+            double percent5 = ((double)numbersUpFrom800 / n) * 100;
+
+            Console.WriteLine($"{percent1:F2}%");
+            Console.WriteLine($"{percent2:F2}%");
+            Console.WriteLine($"{percent3:F2}%");
+            Console.WriteLine($"{percent4:F2}%");
+            Console.WriteLine($"{percent5:F2}%");
+        }
+    }
+}
---
+++ ProgramingBasicsWithC#/E04ForLoop_Exercise_29_05_22/P07TrekkingMania/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace P07TrekkingMania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int groupsN = int.Parse(Console.ReadLine());
+
+            int allThePpl = 0;
+            double pplForMusala = 0;
+            double pplForMonblan = 0;
+            double pplForKilimandjaro = 0;
+            double pplForK2 = 0;
+            double pplForEverest = 0;
+
+            for (int i = 1; i <= groupsN; i++)
+            {
+                int pplInGroup = int.Parse(Console.ReadLine());
+                allThePpl += pplInGroup;
+                if (pplInGroup <= 5)
+                {
+                    pplForMusala += pplInGroup;
+                }
+                else if (pplInGroup >= 6 && pplInGroup <= 12)
+                {
+                    pplForMonblan += pplInGroup;
+                }
+                else if (pplInGroup >= 13 && pplInGroup <= 25)
+                {
+                    pplForKilimandjaro += pplInGroup;
+                }
+                else if (pplInGroup >= 26 && pplInGroup <= 40)
+                {
+                    pplForK2 += pplInGroup;
+                }
+                else
+                {
+                    pplForEverest += pplInGroup;
+                }
+
+            }
+
+            pplForMusala = (pplForMusala / allThePpl) * 100;
+            pplForMonblan = (pplForMonblan / allThePpl) * 100;
+            pplForKilimandjaro = (pplForKilimandjaro / allThePpl) * 100;
+            pplForK2 = (pplForK2 / allThePpl) * 100;
+            pplForEverest = (pplForEverest / allThePpl) * 100;
+
+            Console.WriteLine($"{pplForMusala:F2}%");
+            Console.WriteLine($"{pplForMonblan:F2}%");
+            Console.WriteLine($"{pplForKilimandjaro:F2}%");
+            Console.WriteLine($"{pplForK2:F2}%");
+            Console.WriteLine($"{pplForEverest:F2}%");
+        }
+    }
+}
