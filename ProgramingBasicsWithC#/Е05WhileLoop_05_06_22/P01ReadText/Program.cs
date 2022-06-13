70d0d72a248f0de007f2cf098712b6c2a31588e7
VladislavNedyalkovIvanov <105384381+VladislavNedyalkovIvanov@users.noreply.github.com>
2022-06-13 21:02:46 +0300

    Add files via upload

---
+++ ProgramingBasicsWithC#/Е05WhileLoop_05_06_22/P01ReadText/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P01ReadText
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            while ((name = Console.ReadLine()) != "Stop")
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
---
+++ ProgramingBasicsWithC#/Е05WhileLoop_05_06_22/P05AccountBalance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P05AccountBalance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = 0;
+
+            string any;
+            while ((any = Console.ReadLine()) != "NoMoreMoney")
+            {
+                double increase = double.Parse(any);
+                if (increase >= 0)
+                {
+                    Console.WriteLine($"Increase: {increase:f2}");
+                    money += increase;
+                }
+                else if (increase < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+            }
+            Console.WriteLine($"Total: {money:f2}");
+        }
+    }
+}
