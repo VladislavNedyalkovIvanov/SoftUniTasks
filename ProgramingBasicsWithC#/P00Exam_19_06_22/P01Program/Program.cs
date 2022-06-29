f5de9764c57c5f13ddcae85b2cbf19c18737f29f
VladislavNedyalkovIvanov <105384381+VladislavNedyalkovIvanov@users.noreply.github.com>
2022-06-29 13:37:24 +0300

    Add files via upload

---
+++ ProgramingBasicsWithC#/P00Exam_19_06_22/P01Program/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _1Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double gramFats = 9;
+            double gramProteins = 4;
+            double gramSugars = 4;
+
+            int percentFats = int.Parse(Console.ReadLine());
+            int percentProteins = int.Parse(Console.ReadLine());
+            int percentSugars = int.Parse(Console.ReadLine());
+
+            double percentFatsDouble = (double)percentFats * 0.01;
+            double percentProteinsDouble = (double)percentProteins * 0.01;
+            double percentSugarsDouble = (double)percentSugars * 0.01;
+
+            int allCalories = int.Parse(Console.ReadLine());
+            int percentOfWater = int.Parse(Console.ReadLine());
+            double allCaloriesDouble = allCalories * 1.00;
+
+           
+            double allFats = (allCalories * percentFatsDouble) / gramFats;
+            double allProteins = (allCalories * percentProteinsDouble) / gramProteins;
+            double allSugars = (allCalories * percentSugarsDouble) / gramSugars;
+            double percentOfWaterD = percentOfWater * 0.01;
+
+
+
+            double kgFoods = allFats + allProteins + allSugars;
+            double kgFoodsWithWat = allCaloriesDouble / kgFoods;
+            kgFoodsWithWat -= (kgFoodsWithWat * percentOfWaterD);
+
+            Console.WriteLine($"{kgFoodsWithWat:f4}");
+
+
+        }
+    }
+}
