77c57c7528223ae07e29d7aba44f937da8f84de3
VladislavNedyalkovIvanov <105384381+VladislavNedyalkovIvanov@users.noreply.github.com>
2022-06-29 15:42:52 +0300

    Add files via upload

---
+++ ProgramingBasicsWithC#/E02ConditionalStatements_15_05_22_More/P04TransportPrice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace P04TransportPrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string timeOfDay = Console.ReadLine();
+            double prices = 0;
+
+            if (n < 20)
+            {
+                prices += 0.70;
+                switch (timeOfDay)
+                {
+                    case "day":
+                        prices += 0.79 * n;
+                        break;
+                    case "night":
+                        prices += 0.90 * n;
+                        break;
+                }
+
+            }
+        
+            else if (n >= 20 && n < 100)
+            {
+                prices += 0.09 * n;
+            }
+            else
+            {
+                prices += 0.06 * n;
+            }
+            Console.WriteLine($"{prices:f2}");
+        }
+    }
+}
---
+++ ProgramingBasicsWithC#/E02ConditionalStatements_15_05_22_More/P06FlowerShop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P06FlowerShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceMagnolias = 3.25;
+            double priceZumbuls = 4;
+            double priceRouses = 3.50;
+            double priceCactuses = 8;
+
+            // -5% 
+
+            int magnolias = int.Parse(Console.ReadLine());
+            int zumbuls = int.Parse(Console.ReadLine());
+            int rouses = int.Parse(Console.ReadLine());
+            int cactuses = int.Parse(Console.ReadLine());
+            double priceForGift = double.Parse(Console.ReadLine());
+
+            double allProof = (magnolias * priceMagnolias) + (zumbuls * priceZumbuls) + (rouses * priceRouses) + (cactuses * priceCactuses);
+            allProof = allProof - allProof * 0.05;
+
+            if (allProof >= priceForGift)
+            {
+                double allLeft = allProof - priceForGift;
+                Console.WriteLine($"She is left with {Math.Floor(allLeft)} leva.");
+            }
+            else
+            {
+                double allNeed = priceForGift - allProof;
+                Console.WriteLine($"She will have to borrow {Math.Ceiling(allNeed)} leva.");
+            }
+
+        }
+    }
+}
