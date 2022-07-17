9c0402b97112291de92f9274d347ed5154bf4a15
VladislavNedyalkovIvanov <105384381+VladislavNedyalkovIvanov@users.noreply.github.com>
2022-07-17 22:33:32 +0300

    Update P11FruitShop.js

--- ProgramingBasicsWithJavaScript/E03_1NastedCStateLab_16_07_22/P11FruitShop.js
+++ ProgramingBasicsWithJavaScript/E03_1NastedCStateLab_16_07_22/P11FruitShop.js
@@ -11,54 +11,26 @@ function fruitShop(inbox) {
         case "Thursday":
         case "Friday":
             switch (fruit) {
-                case "banana":
-                    price = 2.50;
-                    break;
-                case "apple":
-                    price = 1.20;
-                    break;
-                case "orange":
-                    price = 0.85;
-                    break;
-                case "grapefruit":
-                    price = 1.45;
-                    break;
-                case "kiwi":
-                    price = 2.70;
-                    break;
-                case "pineapple":
-                    price = 5.50;
-                    break;
-                case "grapes":
-                    price = 3.85;
-                    break;
+                case "banana":  price = 2.50;   break;
+                case "apple":   price = 1.20;   break;
+                case "orange":  price = 0.85;   break;
+                case "grapefruit":  price = 1.45;   break;
+                case "kiwi":    price = 2.70;   break;
+                case "pineapple":   price = 5.50;   break;
+                case "grapes":  price = 3.85;   break;
             }
     }
     switch (dayOfWeek) {
         case "Saturday":
         case "Sunday":
             switch (fruit) {
-                case "banana":
-                    price = 2.70;
-                    break;
-                case "apple":
-                    price = 1.25;
-                    break;
-                case "orange":
-                    price = 0.90;
-                    break;
-                case "grapefruit":
-                    price = 1.60;
-                    break;
-                case "kiwi":
-                    price = 3.00;
-                    break;
-                case "pineapple":
-                    price = 5.60;
-                    break;
-                case "grapes":
-                    price = 4.20;
-                    break;
+                case "banana":  price = 2.70;   break;
+                case "apple":   price = 1.25;   break;
+                case "orange":  price = 0.90;   break;
+                case "grapefruit":  price = 1.60;   break;
+                case "kiwi":    price = 3.00;   break;
+                case "pineapple":   price = 5.60;   break;
+                case "grapes":  price = 4.20;   break;
             }
     }
 if (price != null) {
