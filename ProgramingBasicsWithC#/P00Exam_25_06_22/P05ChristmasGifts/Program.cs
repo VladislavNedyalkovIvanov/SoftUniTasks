48fbb518f1297c59ae7ef7cd93ffc28b970de1d4
VladislavNedyalkovIvanov <vladislav.nedyalkov.ivanov@gmail.com>
2022-10-10 14:39:15 +0300

    Update Program.cs

--- ProgramingBasicsWithC#/P00Exam_25_06_22/P05ChristmasGifts/Program.cs
+++ ProgramingBasicsWithC#/P00Exam_25_06_22/P05ChristmasGifts/Program.cs
@@ -12,7 +12,6 @@ namespace P05ChristmasGifts
 
             int pricePerToy = 5;
             int pricePerPullover = 15;
-				// test for GitHub
             while (yearsAge != "Christmas")
             {
                 if (yearsAge == "Christmas")
